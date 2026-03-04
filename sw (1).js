024b90a0d6b9433180cca7cf1feaffc0b7e0c9d0
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2026-03-04 15:26:25 +0900

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'maia-fnt-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './FNT512.png',
+  './FNT512-transparent.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS.map(url => new Request(url, { cache: 'reload' })));
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type === 'opaque') return response;
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
